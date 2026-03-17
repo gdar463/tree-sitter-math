af4482d3fe821e048dfa49206c905f28bb7e5802
gdar463 <dev@gdar463.com>
2026-03-17 15:59:07 +0100

    fix: change license identifier to the correct SPDX id
    Signed-off-by: gdar463 <dev@gdar463.com>

--- grammar.js
+++ grammar.js
@@ -1,7 +1,7 @@
 /**
  * @file Math grammar for tree-sitter
  * @author gdar463 <dev@gdar463.com>
- * @license GPL-v3.0
+ * @license GPL-3.0-only
  */
 
 /// <reference types="tree-sitter-cli/dsl" />
